eb7e57d6312ff51578a54c0abdf80f0531553e1f
Chris Estes <chris.estes@willowtreeapps.com>
2018-01-28 12:49:04 -0500

    Adding radii to client models.

--- client/src/sprites/baseSprite.ts
+++ client/src/sprites/baseSprite.ts
@@ -2,6 +2,9 @@ import AppSprite from './appSprite'
 import LifeBarSprite from './lifeBarSprite'
 
 export default class BaseSprite extends AppSprite {
+
+    public static readonly RADIUS: number = 120
+
     static loadAsset(game: Phaser.Game): void {
         game.load.image('base', 'base.png')
     }
@@ -11,14 +14,15 @@ export default class BaseSprite extends AppSprite {
     constructor(game: Phaser.Game, x: number, y: number, maxHp: number) {
         super(game, x, y, 'base')
 
-        this.width = 240
-        this.height = 240
+        this.width = BaseSprite.RADIUS
+        this.height = BaseSprite.RADIUS
 
         this.anchor.x = 0.5
         this.anchor.y = 0.5
 
         this.lifeBar = new LifeBarSprite(this.game, 0, -this.height / 2 - 10, maxHp)
         this.addChild(this.lifeBar)
+        this.showHP(maxHp)
     }
 
     showHP(hp: number) {
